7d42619a6543561058ba6cbff21a7dc99ac97fa5
Rhys Sullivan <39114868+RhysSullivan@users.noreply.github.com>
2021-02-04 22:33:03 -0500

    Added tracking to bonks given

--- bot.py
+++ bot.py
@@ -3,6 +3,7 @@ import os
 import io
 import requests
 import discord
+import json
 from PIL import Image
 from dotenv import load_dotenv
 
@@ -19,13 +20,22 @@ class MyClient(discord.Client):
     async def on_message(self, message):
         if self.user == message.author:
             return
-            
-        if '!bonk' in message.content:             
+
+        if '!bonks' in message.content:
+            for user in message.mentions:
+                usersDict = json.load(open("dictionary.txt"))
+                if str(user.id) in usersDict:
+                    numBonks = usersDict[str(user.id)]
+                    response = "{0} has been bonked {1} times".format(user.name, numBonks)
+                    await message.channel.send(response)
+
+        elif '!bonk' in message.content:
             for user in message.mentions:
                 r = requests.get(user.avatar_url, stream=True)
                 if r.status_code == 200:
                     background = Image.open(io.BytesIO(r.content)) # Download the profile picture directly to memory                    
-                    #background.save(os.path.join('profile.png'), quality=85) # CODE TO SAVE THE FILE                    
+                    #background.save(os.path.join('profile.png'), quality=85) # CODE TO SAVE THE FILE
+                    print(user.id)
                     print(user.avatar_url)                    
                     gif = Image.open('Media/TransparentBonkGif.gif')                                        
                     background = background.convert("RGBA")
@@ -40,5 +50,16 @@ class MyClient(discord.Client):
                     images[0].save(arr, format='GIF', save_all=True, append_images=images[1:], duration=100, loop=0)
                     arr.seek(0)
                     await message.channel.send(file = discord.File(arr, 'bonk.gif'))
+
+                    usersDict = json.load(open("dictionary.txt"))
+
+                    if str(user.id) in usersDict:
+                        usersDict[str(user.id)] = usersDict[str(user.id)] + 1
+                    else:
+                        usersDict[str(user.id)] = 1
+                    json.dump(usersDict, open("dictionary.txt", "w"))
+
+
+
 client = MyClient()
 client.run(TOKEN)
