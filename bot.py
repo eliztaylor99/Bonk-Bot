6beb4a8f716cfc1d98157bb5bf7936b18fb20725
Rhys Sullivan <39114868+RhysSullivan@users.noreply.github.com>
2021-02-16 16:20:42 -0500

    Updated to test with 8 bit range for background in gifs

--- bot.py
+++ bot.py
@@ -42,7 +42,7 @@ class MyClient(discord.Client):
         if self.user == message.author:
             return
         if 'version' in message.content:
-            await message.channel.send("0.2.0")
+            await message.channel.send("0.2.1")
         if '!bonkboard' in message.content:
             usersDict = json.load(open("dictionary.txt"))
             allBonks = []
@@ -72,6 +72,7 @@ class MyClient(discord.Client):
                 r = requests.get(user.avatar_url, stream=True)
                 if r.status_code == 200:
                     background = Image.open(io.BytesIO(r.content)) # Download the profile picture directly to memory                    
+                    background = background.convert("P", pallete=Image.ADAPTIVE, colors=8)
                     #background.save(os.path.join('profile.png'), quality=85) # CODE TO SAVE THE FILE
                     print(user.id)
                     print(user.avatar_url)                    
@@ -85,9 +86,9 @@ class MyClient(discord.Client):
                         images.append(Image.alpha_composite(background, gif.resize(imgSize).convert("RGBA")))                        
                         gif.seek(0)
                     arr = io.BytesIO()
-                    images[0].save(arr, save_all=True, append_images=images[1:], duration=100, loop=0)
+                    images[0].save(arr, format='GIF', save_all=True, append_images=images[1:], duration=100, loop=0)
                     arr.seek(0)
-                    await message.channel.send(file = discord.File(arr, 'bonk.webp'))
+                    await message.channel.send(file = discord.File(arr, 'bonk.gif'))
 
                     usersDict = json.load(open("dictionary.txt"))
 
